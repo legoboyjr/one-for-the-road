b183c402a3ae10adf77059ef2d0fe4677fcf6603
Alexander Thompson <legoboyjr@gmail.com>
2018-12-05 20:18:42 -0500

    update

--- server/models/note.js
+++ server/models/note.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
+const uuid = require('uuid');
 const noteSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        default: uuid.v4
+    },
     text: {
         type: String,
         required: true
