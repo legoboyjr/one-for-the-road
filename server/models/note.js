c0bf6af71f481b47c3fc7cd5bca8b4dbdc9213d1
Alexander Thompson <legoboyjr@gmail.com>
2018-12-05 19:04:27 -0500

    added mongoose model for database updated the notes routes

--- server/models/note.js
+++ server/models/note.js
@@ -1,63 +1,23 @@
-const uuid = require('uuid');
+const mongoose = require('mongoose');
+const noteSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        required: true
+    },
+    coords: {
+        type: Object,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+});
 
-const notes = {};
-const ids = [];
-
-class Note {
-  constructor(data){
-    this.text = data.text; 
-    this.coords = data.coords;
-    this.id = uuid.v4();
-  }
-
-  save(){
-    notes[this.id] = this;
-    ids.push(this.id);
-    return this;
-  }
-  static _asArray() {
-    return ids.map(id => notes[id]);
-  }
-  static findById(id) {
-    return notes[id] || {};
-  }
-
-  static find(query) {
-    if(!query) return ids.map(id => notes[id]);
-    if(query.id) return [this.findById(query.id)];
-    if(query.coords) return this._asArray()
-      .filter(note => 
-        (note.coords.latitude === query.coords.latitude) &&
-                (note.coords.longitue === query.coords.longitude));
-    return [];
-  }
-
-  static findOne(query) {
-    if(!query) return {};
-    if(query.id) return this.findById(query.id);
-    if(query.coords) return this._asArray()
-      .find(note => 
-        (note.coords.latitude === query.coords.latitude) &&
-                (note.coords.longitude === query.coords.longitude));
-    return {};
-  }
-  static findByIdAndUpdate(id, updates, options) {
-    const old = notes[id]; 
-    // should address if the note isn't found
-    notes[id] = Object.assign({}, old, updates);
-    return options.new ? notes[id] : old;
-
-  }
-
-  static findByIdAndRemove(id) {
-    const note = notes[id];
-    delete notes[id];
-    const idx = ids.findIndex(e => e === id);
-    ids.splice(idx, 1);
-    return note;
-  }
-
-}
-
-
-module.exports = Note;
+module.exports = mongoose.model('Note', noteSchema);
--- server/routers/notes.js
+++ server/routers/notes.js
@@ -3,37 +3,56 @@ const router = express.Router();
 const Note = require('../models/note');
 
 
-router.get('/', (req, res) => {
-  const notes = Note.find();
-  res.status(200).json({ notes: notes });
+router.get('/', async (req, res) => {
+  try {
+    const notes = await Note.find();
+    res.status(200).json({ notes: notes });
+  } catch (e) {
+    res.status(500).json({ message: 'Server Error' });
+  }
+
 });
 
-router.get('/:id', (req, res) => {
-  const note = Note.findById(req.params.id);
-  res.status(200).json({ note: note });
+router.get('/:id', async (req, res) => {
+  try {
+    const note = await Note.findById(req.params.id);
+    res.status(200).json({ note: note });
+  } catch (e) {
+    res.status(500).json({ message: 'Server Error' });
+  }
 });
 
-router.post('/', (req, res) => {
-  const { text, coords } = req.body;
-  const note = new Note({ text, coords });
-  note.save();
-  res.status(201).json({ message: 'successful' });
+router.post('/', async (req, res) => {
+  try {
+    const { text, coords } = req.body;
+    const note = new Note({ text, coords });
+    await note.save();
+    res.status(201).json({ message: 'successful' });
+  } catch (e) {
+    res.status(500).json({ message: 'Server Error' });
+  }
 });
 
-router.put('/:id', (req, res) => {
-  const { id } = req.params;
-  const { text } = req.body;
-  Note.findByIdAndUpdate(id, { text: text });
-  res.status(200).json({ message: 'successful'});
+router.put('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { text } = req.body;
+    await Note.findByIdAndUpdate(id, { text: text });
+    res.status(200).json({ message: 'successful' });
+  } catch (e) {
+    res.status(500).json({ message: 'Server Error' });
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  const { id } = req.params;
-  Note.findByIdAndRemove(id);
-  res.status(200).json({ message: 'successfully deleted' });
+router.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Note.findByIdAndRemove(id);
+    res.status(200).json({ message: 'successfully deleted' });
+  } catch (e) {
+    res.status(500).json({ message: 'Server Error' });
+  }
 });
-
-
 
 
 module.exports = router;
